130d8040fdf89619694e8341eee680581660d6d6
Michael Matus <michael@matus.at>
2023-04-13 19:58:33 +0200

    Add project files.

---
+++ Bev.IO.RemoteInterface/IRemoteInterface.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Bev.IO.RemoteInterface
+{
+    public interface IRemoteInterface
+    {
+        void Output(int address, string command);
+
+        string Enter(int address);
+
+        void Remote(int address);
+
+        void Local(int address);
+
+        void Trigger(int address);
+    }
+}
